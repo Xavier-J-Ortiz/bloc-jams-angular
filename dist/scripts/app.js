f77008171793a1fcb80152e6b555a10a19ba83ad
xortiz <cuchumino@gmail.com>
2016-12-26 20:04:16 -0500

    added state references in index.html, collection.html, and album.html. Jumping from site to site, feels very smooth and snappy!

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -5,17 +5,19 @@
              enabled: true,
              requireBase: false
          });
-
          $stateProvider
-         .state('landing', {
-             url: '/',
-             templateUrl: '/templates/landing.html'
-         })
-         .state('album', {
-             url: '/album',
-             templateUrl: '/templates/album.html'
-         });
-
+             .state('landing', {
+                 url: '/',
+                 templateUrl: '/templates/landing.html'
+             })
+             .state('album', {
+                 url: '/album',
+                 templateUrl: '/templates/album.html'
+             })
+             .state('collection', {
+                 url: '/collection',
+                 templateUrl: '/templates/collection.html'
+             });
      }
      angular
          .module('blocJams', ['ui.router'])
