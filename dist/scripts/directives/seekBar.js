b036a2d1a813d4af73b0e8456cb31e70688d3df4
xortiz <cuchumino@gmail.com>
2017-01-02 19:03:38 -0500

    added the correct time format of mm:ss. Noticed that click on the seekbar doesn't work. However, dragging and dropping the thumb works and tracks song current time, as well as volume. Also, when a song starts, the initial volume does not update.

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -59,13 +59,12 @@
                              scope.value = percent * scope.max;
                              notifyOnChange(scope.value);
                          });
-                     });
-                     
+                     });                     
                      var notifyOnChange = function(newValue) {
                          if (typeof scope.onChange === 'function') {
                              scope.onChange({value: newValue});
                          }
- };
+                     };
                      
                      $document.bind('mouseup.thumb', function() {
                          $document.unbind('mousemove.thumb');
